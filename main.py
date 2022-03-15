8538b81d158ad2065ebfba6d326a2fe64e469adc
Juan Pablo <jpmontoya19@gmail.com>
2022-03-14 20:38:16 -0500

    Add: Counting filtered pokemon names

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-
+from count_filter_names import count_filter_names
 
 def main():
-    pass
+    url = "https://pokeapi.co/api/v2/pokemon/"
+    print(count_filter_names(url))
 
 if __name__ == '__main__':
     main()
