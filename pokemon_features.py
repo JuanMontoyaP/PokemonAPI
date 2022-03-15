e7e1c7ec4f0630071d0af1f03eb8831cbf23be06
Juan Pablo <jpmontoya19@gmail.com>
2022-03-15 13:03:35 -0500

    Update: Get the pokemon url API to pokemon feature file

--- pokemon_features.py
+++ pokemon_features.py
@@ -1,22 +1,37 @@
-from functions import key_value_json
 from read_api import read_api
 import sys
 
 def get_pokemon_features(pokemons, feature="name"):
-    """Return a list of features from different pokemons. Pokemons must be a dictionary or a json"""
+    """Return a list of features from different pokemons. Pokemons must be a dictionary list"""
     try :
         return [pokemon[feature] for pokemon in pokemons]
     except KeyError as error:
         print(f'The key {error} does not exist in the dictionary.')
         sys.exit()
 
-def main():
+
+def get_pokemon_url(pokemon_name):
+    """Returns the url of the pokemon API."""
     url = "https://pokeapi.co/api/v2/pokemon/"
-    response = read_api(url)
+    return url + pokemon_name
 
-    pokemons = key_value_json(response, 'results')
 
-    print(get_pokemon_features(pokemons))
+def main():
+    # url = "https://pokeapi.co/api/v2/pokemon/"
+    # response = read_api(url)
+
+    # pokemons = key_value_json(response, 'results')
+
+    # print(get_pokemon_features(pokemons))
+
+    pokemon = "raichu"
+    url = get_pokemon_url(pokemon)
+
+    poke = read_api(url)
+
+    print(poke.keys())
+
+    print(poke['name'])
 
 if __name__ == "__main__":
     main()
