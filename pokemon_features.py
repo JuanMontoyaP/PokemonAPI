8ef17ab85cfe1473b48fb26273165b941855b4ce
Juan Pablo <jpmontoya19@gmail.com>
2022-03-15 14:38:51 -0500

    Add: Getting specie url

--- pokemon_features.py
+++ pokemon_features.py
@@ -1,3 +1,4 @@
+from functions import key_value_json
 from read_api import read_api
 import sys
 
@@ -16,6 +17,20 @@ def get_pokemon_url(pokemon_name):
     return url + pokemon_name
 
 
+def get_pokemon_specie_url(pokemon_name):
+    """Retrun the specie url API which the pokemon belongs to"""
+    url = get_pokemon_url(pokemon_name)
+    
+    pokemon = read_api(url)
+
+    pokemon_specie = key_value_json(pokemon, "species")
+
+    return pokemon_specie['url']
+
+def get_egg_group(pokemon_name):
+    pass
+
+
 def main():
     # url = "https://pokeapi.co/api/v2/pokemon/"
     # response = read_api(url)
@@ -24,14 +39,17 @@ def main():
 
     # print(get_pokemon_features(pokemons))
 
-    pokemon = "raichu"
-    url = get_pokemon_url(pokemon)
+    # pokemon = "raichu"
+    # url = get_pokemon_url(pokemon)
 
-    poke = read_api(url)
+    # poke = read_api(url)
 
-    print(poke.keys())
+    # print(poke.keys())
 
-    print(poke['name'])
+    # print(poke['name'])
+
+    print(get_pokemon_specie_url("raichu"))
+    
 
 if __name__ == "__main__":
     main()
