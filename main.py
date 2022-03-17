37449f1e266250ee3f3d737e121f978479d7ce69
Juan Pablo <jpmontoya19@gmail.com>
2022-03-16 19:19:06 -0500

    Add: Last function

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from count_filter_names import count_filter_names
 from procreate_pokemons import how_many_species_can_procreate
+from pokemon_weight import get_max_min_weight
 
 def main():
-    print(count_filter_names())
+    print(count_filter_names()) # First question.
 
-    print(how_many_species_can_procreate())
+    print(how_many_species_can_procreate()) # Second question.
+
+    print(get_max_min_weight()) # Third question.
 
 if __name__ == '__main__':
     main()
